235fc43de7130c7fa5a563d4310bd86b26a3cbfd
Sneha Rajeshirke <rajeshirkesneha22@gmail.com>
2024-04-01 22:46:30 +0530

    Modified app.js

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors({
     credentials: true, 
-    origin: 'http://localhost:3000', 
+    origin: 'https://rentwheel-ecyw.onrender.com/', 
 }));
 
 // Configuring ENV files and connection files
